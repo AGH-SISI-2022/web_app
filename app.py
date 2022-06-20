9a0f7773735a0cf99aafd1d14bfc025f33e2a324
Kamil Koczera <Koczera9@wp.pl>
2022-06-20 17:50:44 +0200

    Work simulation changes

--- app.py
+++ app.py
@@ -1,35 +1,48 @@
-from flask import Flask, request
-import requests
+from flask import Flask, request, jsonify
+import requests, time
 
 app = Flask(__name__)
 
-url = "management.default.svc.cluster.local:8080" # todo: add path to url
+url = "management.default.svc.cluster.local:8080"
 
 @app.route('/')
 def hello():
     return '<h1>Hello, World!</h1>'
+
+@app.route('/send', methods=['POST'])
+def send():
+    requests.post(url=url + '/send', data=request.get_json())
+
+    resp = jsonify(success=True)
+    return resp
 
 @app.route('/watch', methods=['POST'])
 def watch():
     request_data = request.get_json()
     print('request data:', request_data)
 
-    requests.post(url=url, data=request_data)
-
     video_time = int(request_data['video_time'])
+
+    simulate_time = 0
 
     # simulate work:
     if (video_time < 1000):
-        for i in range(1000):
-            i*i
-    elif (video_time < 10000):
-        for i in range(10000):
-            i*i
-    else:
-        for i in range(100000):
-            i*i
+        simulate_time = 2
 
-    return request_data
+    elif (video_time < 100000):
+        simulate_time = 5
+
+    else:
+        simulate_time = 10
+
+    timeout = time.time() + float(simulate_time)
+
+    while True:
+        if time.time() > timeout:
+            break
+
+    resp = jsonify(success=True)
+    return resp
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)
